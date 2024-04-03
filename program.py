c26ccb2ed3982ca2d7a4bea42dc99797ea40fb93
Ashweeni Bharde <130930038+ash-504@users.noreply.github.com>
2024-04-03 23:36:34 +0530

    Add files via upload
    This program.py is a Python program that performs detection and recognition of faces.

---
+++ program.py
@@ -0,0 +1,89 @@
+import face_recognition
+import cv2
+import numpy as np
+import csv
+import os
+from datetime import datetime
+ 
+video_capture = cv2.VideoCapture(0)
+ 
+ash_image = face_recognition.load_image_file('photos/image1.jpeg')
+ash_encoding = face_recognition.face_encodings(ash_image)[0]
+
+ved_image = face_recognition.load_image_file('photos/image2.jpeg')
+ved_encoding = face_recognition.face_encodings(ved_image)[0]
+
+vini_image = face_recognition.load_image_file('photos/image3.jpeg')
+vini_encoding = face_recognition.face_encodings(vini_image)[0]
+
+known_face_encodings = [ 
+    ash_encoding,
+    ved_encoding,
+    vini_encoding
+]
+
+known_face_names = [
+    'ash',
+    'ved',
+    'vini'
+]
+ 
+students = known_face_names.copy()
+ 
+face_locations = []
+face_encodings = []
+face_names = []
+s=True
+ 
+ 
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+ 
+f = open(current_date+'.csv','w+',newline = '')
+lnwriter = csv.writer(f)
+ 
+while True:
+    _,frame = video_capture.read()
+    small_frame = cv2.resize(frame,(0,0),fx=0.25,fy=0.25)
+    rgb_small_frame = small_frame[:,:,::-1]
+    if s:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings,face_encoding)
+            name=""
+            face_distance = face_recognition.face_distance(known_face_encodings,face_encoding)
+            best_match_index = np.argmin(face_distance)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+ 
+            face_names.append(name)
+            if name in known_face_names:
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                bottomLeftCornerOfText = (10,100)
+                fontScale              = 1.5
+                fontColor              = (255,0,0)
+                thickness              = 3
+                lineType               = 2
+ 
+                cv2.putText(frame,name+' Present', 
+                    bottomLeftCornerOfText, 
+                    font, 
+                    fontScale,
+                    fontColor,
+                    thickness,
+                    lineType)
+ 
+                if name in students:
+                    students.remove(name)
+                    print(students)
+                    current_time = now.strftime("%H-%M-%S")
+                    lnwriter.writerow([name,current_time])
+    cv2.imshow("attendence system",frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+ 
+video_capture.release()
+cv2.destroyAllWindows()
+f.close()
